74170695d06a287fcce0e460b46d306e38ed135e
Abhyuday Vaish <63145440+Abhyuday-06@users.noreply.github.com>
2025-07-11 09:36:28 +0530

    Update add2.asm

--- add2.asm
+++ add2.asm
@@ -1,7 +1,7 @@
 ORG 0000H
 MOV A, #31H
 MOV B, #29H
-ADDC A, B
+ADDC A, B ; Each memory location can have a maximum of 2 hexadecimal digits. This is why we're adding 8531 and 2927 in two steps
 MOV 30H, A
 
 MOV A, #85H
