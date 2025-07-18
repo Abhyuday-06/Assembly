b511658d946f36f8fc9952bca14188a928fbf05b
Abhyuday Vaish <63145440+Abhyuday-06@users.noreply.github.com>
2025-07-18 08:40:50 +0530

    Create exchange1.asm

---
+++ Average, Exchange and Largest Number/exchange1.asm
@@ -0,0 +1,15 @@
+ORG 0000H
+;Store the number of iterations in 40 location
+MOV R0, #40H 
+MOV R1, #51H
+MOV A, @R0 ; @ means to access the data at the R0 location. Direct transfer of data between registers in not possible. Therefore, Register -> Accumulator -> Register
+MOV R2, A ; R2 is used as a counter here for 5 iterations
+INC R0 ; Increment moves to the next address eg: 41, 42, 43.....
+L1: MOV A, @R0 ; Data at R0 location is moved to accumulator. L1 is loop block
+XCH A, @R1 ; Exchange of data between R1 and Accumulator(having R0's data) takes place
+MOV @R0, A ; Move Accumulator's data to R0 location
+INC R0; Increment R0
+INC R1; Increment R1
+DJNZ R2, L1 # Does the decrement and checks if it's zero or not. DJNZ = Decrement and Jump, Not Zero?
+
+END
