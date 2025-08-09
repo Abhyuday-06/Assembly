f20c1e3be85fb0134a30f64e3ad69cf371e933d5
Abhyuday Vaish <63145440+Abhyuday-06@users.noreply.github.com>
2025-08-08 20:25:32 -0700

    Update led_hardware1.asm

--- LED_Hardware/led_hardware1.asm
+++ LED_Hardware/led_hardware1.asm
@@ -2,12 +2,12 @@
 ; set XTAL = 33 MHz. In the same window(right side) -> Target option -> select on chip xram, on chip rom, multiple dptr
 ; same window, there will be output option, create hex file
 
-; there will be one debug option in the same window -> by default software option is selected -> select simulator for software project
-; on the right side on debug -> in Use -> Keil Monitor 51
+; there will be one debug option in the same window -> by default software option is selected -> select simulator for software project(don't select anything for hardware)
+; on the right side on debug -> in Use -> Keil Monitor 51 Driver
 ; now go to settings option -> select Port (check COM from Device Manager). 
 ; USB Serial port will show up when hardware is connected. COM will also be mentioned there
 ; Baud rate=38400
-; after saving file -> click only rebuild
+; after saving file -> click only rebuild -> RESET -> Debug -> RESET -> Run
 
 	ORG 0000H
 	MOV P1, #00H
